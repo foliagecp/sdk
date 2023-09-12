37c9a3e81f185608495b4f6fcb30546c1a27a8bf
Andrei Shipov <andreishipov@hotmail.com>
2023-09-12 16:10:18 +0300

    update: rewritten using flag lib for args

--- tests/main.go
+++ tests/main.go
@@ -1,38 +1,31 @@
-
-
-// Foliage tests main package.
-// Provides configurable running of different test samples that goes along with the SDK.
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 
 	"github.com/foliagecp/sdk/tests/basic"
 )
 
-func helpInfo() {
-	fmt.Println("usage: tests <test_name>")
-	fmt.Println("This help: tests -h | --help")
-}
-
-func missingTestInfo(testName string) {
-	fmt.Printf("Test named \"%s\" not found!\n", testName)
-}
-
 func main() {
-	// TODO: use existing libraries for reading args
-	argsWithoutProg := os.Args[1:]
-	if len(argsWithoutProg) == 0 || argsWithoutProg[0] == "-h" || argsWithoutProg[0] == "--help" {
-		helpInfo()
+	helpFlag := flag.Bool("h", false, "Show help message")
+	helpFlagAlias := flag.Bool("help", false, "Show help message (alias)")
+
+	flag.Parse()
+
+	if *helpFlag || *helpFlagAlias || flag.NArg() == 0 {
+		fmt.Println("usage: tests <test_name>")
+		flag.PrintDefaults()
 		return
 	}
-	testName := argsWithoutProg[0]
+
+	testName := flag.Arg(0)
+
 	switch testName {
 	case "basic":
 		defer basic.Start()
 	default:
-		missingTestInfo(testName)
+		fmt.Printf("Test named \"%s\" not found!\n", testName)
 		return
 	}
 }
