00575bdef48e996874dd1a03a9c29bc2da03a311
Andrei Shipov <andreishipov@hotmail.com>
2023-12-23 04:30:51 +0900

    IdHandlers limit removed, can be really huge amount of vertices created at once for example

--- embedded/graph/crud/common.go
+++ embedded/graph/crud/common.go
@@ -11,28 +11,28 @@ var (
 
 func RegisterAllFunctionTypes(runtime *statefun.Runtime) {
 	// High-Level API Registration
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.type.create", CreateType, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.type.update", UpdateType, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.type.delete", DeleteType, *statefun.NewFunctionTypeConfig().SetServiceState(true))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.type.create", CreateType, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.type.update", UpdateType, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.type.delete", DeleteType, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
 
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.types.link.create", CreateTypesLink, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.types.link.update", UpdateTypesLink, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.types.link.delete", DeleteTypesLink, *statefun.NewFunctionTypeConfig().SetServiceState(true))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.types.link.create", CreateTypesLink, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.types.link.update", UpdateTypesLink, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.types.link.delete", DeleteTypesLink, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
 
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.object.create", CreateObject, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.object.update", UpdateObject, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.object.delete", DeleteObject, *statefun.NewFunctionTypeConfig().SetServiceState(true))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.object.create", CreateObject, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.object.update", UpdateObject, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.object.delete", DeleteObject, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
 
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.objects.link.create", CreateObjectsLink, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.objects.link.update", UpdateObjectsLink, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, "functions.cmdb.api.objects.link.delete", DeleteObjectsLink, *statefun.NewFunctionTypeConfig().SetServiceState(true))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.objects.link.create", CreateObjectsLink, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.objects.link.update", UpdateObjectsLink, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, "functions.cmdb.api.objects.link.delete", DeleteObjectsLink, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
 
 	// Low-Level API Registration
-	statefun.NewFunctionType(runtime, llAPIVertexCUDNames[0], LLAPIVertexCreate, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, llAPIVertexCUDNames[1], LLAPIVertexUpdate, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, llAPIVertexCUDNames[2], LLAPIVertexDelete, *statefun.NewFunctionTypeConfig().SetServiceState(true))
+	statefun.NewFunctionType(runtime, llAPIVertexCUDNames[0], LLAPIVertexCreate, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, llAPIVertexCUDNames[1], LLAPIVertexUpdate, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, llAPIVertexCUDNames[2], LLAPIVertexDelete, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
 
-	statefun.NewFunctionType(runtime, llAPILinkCUDNames[0], LLAPILinkCreate, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, llAPILinkCUDNames[1], LLAPILinkUpdate, *statefun.NewFunctionTypeConfig().SetServiceState(true))
-	statefun.NewFunctionType(runtime, llAPILinkCUDNames[2], LLAPILinkDelete, *statefun.NewFunctionTypeConfig().SetServiceState(true))
+	statefun.NewFunctionType(runtime, llAPILinkCUDNames[0], LLAPILinkCreate, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, llAPILinkCUDNames[1], LLAPILinkUpdate, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
+	statefun.NewFunctionType(runtime, llAPILinkCUDNames[2], LLAPILinkDelete, *statefun.NewFunctionTypeConfig().SetServiceState(true).SetMaxIdHandlers(-1))
 }
