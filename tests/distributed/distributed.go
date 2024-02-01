2ecc3d1c2ce51697621390a0e8b41f9ca0509f85
Andrei Shipov <andreishipov@hotmail.com>
2024-02-02 00:25:44 +0900

    sharding tests ready

--- tests/distributed/distributed.go
+++ tests/distributed/distributed.go
@@ -3,12 +3,15 @@
 package main
 
 import (
-	"time"
+	/*"time"
 
 	"github.com/foliagecp/easyjson"
+	"github.com/nats-io/nats.go"
 	"github.com/prometheus/client_golang/prometheus"
 
 	graphCRUD "github.com/foliagecp/sdk/embedded/graph/crud"
+
+
 	lg "github.com/foliagecp/sdk/statefun/logger"
 
 	// Comment out and no not use graphDebug for resolving the cgo conflict between go-graphviz and rogchap (when --ldflags '-extldflags "-Wl,--allow-multiple-definition"' does not help)
@@ -18,21 +21,27 @@ import (
 	statefun "github.com/foliagecp/sdk/statefun"
 	"github.com/foliagecp/sdk/statefun/cache"
 	sfPlugins "github.com/foliagecp/sdk/statefun/plugins"
+	"github.com/foliagecp/sdk/statefun/system"*/
+
+	lg "github.com/foliagecp/sdk/statefun/logger"
+	"github.com/foliagecp/sdk/statefun/sharding"
 	"github.com/foliagecp/sdk/statefun/system"
+	"github.com/nats-io/nats.go"
+	"time"
 )
 
 var (
 	// NatsURL - nats server url
 	NatsURL string = system.GetEnvMustProceed("NATS_URL", "nats://nats:foliage@nats:4222")
-	// MasterFunctionContextIncrement - does the master stateful function do the increment operation on each call in its context
+	/*// MasterFunctionContextIncrement - does the master stateful function do the increment operation on each call in its context
 	MasterFunctionContextIncrement bool = system.GetEnvMustProceed("MASTER_FUNC_CONTEXT_INCREMENT", true)
 	// MasterFunctionContextIncrementOption - Default increment value
 	MasterFunctionContextIncrementOption int = system.GetEnvMustProceed("MASTER_FUNC_CONTEXT_INCREMENT_OPTION", 1)
 	// MasterFunctionLogs - enable logging of the master function
-	MasterFunctionLogs bool = system.GetEnvMustProceed("MASTER_FUNC_LOGS", true)
+	MasterFunctionLogs bool = system.GetEnvMustProceed("MASTER_FUNC_LOGS", true)*/
 )
 
-func MasterFunction(executor sfPlugins.StatefunExecutor, contextProcessor *sfPlugins.StatefunContextProcessor) {
+/*func MasterFunction(executor sfPlugins.StatefunExecutor, contextProcessor *sfPlugins.StatefunContextProcessor) {
 	start := time.Now()
 
 	var functionContext *easyjson.JSON
@@ -80,10 +89,10 @@ func RegisterFunctionTypes(runtime *statefun.Runtime) {
 	graphTX.RegisterAllFunctionTypes(runtime)
 	graphDebug.RegisterAllFunctionTypes(runtime)
 	jpgql.RegisterAllFunctionTypes(runtime, 30)
-}
+}*/
 
 func Start() {
-	system.GlobalPrometrics = system.NewPrometrics("", ":9901")
+	/*system.GlobalPrometrics = system.NewPrometrics("", ":9901")
 
 	afterStart := func(runtime *statefun.Runtime) error {
 		CreateTestGraph(runtime)
@@ -97,6 +106,27 @@ func Start() {
 		}
 	} else {
 		lg.Logf(lg.ErrorLevel, "Cannot create statefun runtime due to an error: %s\n", err)
+	}*/
+
+	nc, err := nats.Connect(NatsURL)
+	if err != nil {
+		return
+	}
+
+	js, err := nc.JetStream(nats.PublishAsyncMaxPending(256))
+	if err != nil {
+		return
+	}
+
+	s, e := sharding.NewShard(nc, js, "hub")
+	if e == nil {
+		s.Start()
+	} else {
+		lg.Logln(lg.ErrorLevel, e)
+	}
+
+	for true {
+		time.Sleep(1 * time.Second)
 	}
 }
 
