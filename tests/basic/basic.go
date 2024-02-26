15d2640f4e59348f40111fe3183c65050a917a5a
Andrei Shipov <andreishipov@hotmail.com>
2024-02-26 20:27:54 +0900

    restored to original after build contrains solved double cgo inclusion issue

--- tests/basic/basic.go
+++ tests/basic/basic.go
@@ -13,7 +13,7 @@ import (
 	lg "github.com/foliagecp/sdk/statefun/logger"
 
 	// Comment out and no not use graphDebug for resolving the cgo conflict between go-graphviz and rogchap (when --ldflags '-extldflags "-Wl,--allow-multiple-definition"' does not help)
-	//graphDebug "github.com/foliagecp/sdk/embedded/graph/debug"
+	graphDebug "github.com/foliagecp/sdk/embedded/graph/debug"
 	"github.com/foliagecp/sdk/embedded/graph/jpgql"
 	statefun "github.com/foliagecp/sdk/statefun"
 	"github.com/foliagecp/sdk/statefun/cache"
@@ -132,7 +132,7 @@ func RegisterFunctionTypes(runtime *statefun.Runtime) {
 	}
 
 	graphCRUD.RegisterAllFunctionTypes(runtime)
-	//graphDebug.RegisterAllFunctionTypes(runtime)
+	graphDebug.RegisterAllFunctionTypes(runtime)
 	jpgql.RegisterAllFunctionTypes(runtime)
 }
 
