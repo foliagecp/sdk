4d8fd0a45894f4d5e476b2f07e5590ea8246768e
Andrei Shipov <andreishipov@hotmail.com>
2024-03-26 22:49:59 +0900

    updates: client location changed; updates and fixes; trigger functions

---
+++ clients/go/db/cmdb.go
@@ -0,0 +1,369 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/foliagecp/easyjson"
+	sfMediators "github.com/foliagecp/sdk/statefun/mediator"
+	sfp "github.com/foliagecp/sdk/statefun/plugins"
+	"github.com/nats-io/nats.go"
+)
+
+type TriggerType = string
+
+const (
+	CreateTrigger TriggerType = "create"
+	UpdateTrigger             = "update"
+	DeleteTrigger             = "delete"
+	ReadTrigger               = "read"
+)
+
+type CMDBSyncClient struct {
+	request sfp.SFRequestFunc
+}
+
+func NewCMDBSyncClient(NatsURL string, NatsRequestTimeout int) (CMDBSyncClient, error) {
+	var err error
+	nc, err := nats.Connect(NatsURL)
+	if err != nil {
+		return CMDBSyncClient{}, err
+	}
+	request := getRequestFunc(nc, NatsRequestTimeout)
+	return NewCMDBSyncClientFromRequestFunction(request)
+}
+
+/*
+ctx.Request
+// or
+runtime.Request
+*/
+func NewCMDBSyncClientFromRequestFunction(request sfp.SFRequestFunc) (CMDBSyncClient, error) {
+	if request == nil {
+		return CMDBSyncClient{}, fmt.Errorf("request must not be nil")
+	}
+	return CMDBSyncClient{request: request}, nil
+}
+
+// ------------------------------------------------------------------------------------------------
+
+func (cmdb CMDBSyncClient) commonTriggerDelete(body easyjson.JSON, triggerType TriggerType, statefunName ...string) easyjson.JSON {
+	triggerPath := fmt.Sprintf("triggers.%s", triggerType)
+	var bodyTriggers easyjson.JSON
+	if body.GetByPath(triggerPath).IsNonEmptyObject() {
+		newTriggers := []string{}
+		if arr, ok := body.GetByPath(triggerPath).AsArrayString(); ok {
+			for _, sf := range arr {
+				toRemove := false
+				for _, sf2Remove := range statefunName {
+					if sf == sf2Remove {
+						toRemove = true
+					}
+				}
+				if !toRemove {
+					newTriggers = append(newTriggers, sf)
+				}
+			}
+		}
+		bodyTriggers = easyjson.NewJSONObjectWithKeyValue(triggerPath, easyjson.JSONFromArray(newTriggers))
+	} else {
+		bodyTriggers = easyjson.NewJSONObjectWithKeyValue(triggerPath, easyjson.NewJSONArray())
+	}
+
+	body.SetByPath(triggerPath, bodyTriggers)
+	newBody := body.GetByPath("body")
+	if newBody.IsNull() {
+		newBody = easyjson.NewJSONObject()
+	}
+
+	return newBody
+}
+
+func (cmdb CMDBSyncClient) commonTriggersDrop(body easyjson.JSON, triggerType TriggerType) easyjson.JSON {
+	triggerPath := fmt.Sprintf("triggers.%s", triggerType)
+
+	body.SetByPath(triggerPath, easyjson.NewJSONArray())
+	newBody := body.GetByPath("body")
+	if newBody.IsNull() {
+		newBody = easyjson.NewJSONObject()
+	}
+
+	return newBody
+}
+
+func (cmdb CMDBSyncClient) TriggerObjectSet(typeName string, triggerType TriggerType, statefunName ...string) error {
+	if len(statefunName) == 0 {
+		return fmt.Errorf("at least 1 statefun name is required")
+	}
+	body := easyjson.NewJSONObject()
+	body.SetByPath(fmt.Sprintf("triggers.%s", triggerType), easyjson.JSONFromArray(statefunName))
+	return cmdb.TypeUpdate(
+		typeName,
+		body,
+		false,
+	)
+}
+
+func (cmdb CMDBSyncClient) TriggerObjectDelete(typeName string, triggerType TriggerType, statefunName ...string) error {
+	if len(statefunName) == 0 {
+		return fmt.Errorf("at least 1 statefun name is required")
+	}
+
+	data, err := cmdb.TypeRead(typeName)
+	if err != nil {
+		return err
+	}
+
+	body := data.GetByPath("body")
+	if !body.IsNull() {
+		body = cmdb.commonTriggerDelete(body, triggerType, statefunName...)
+	}
+
+	return cmdb.TypeUpdate(
+		typeName,
+		body,
+		true,
+	)
+}
+
+func (cmdb CMDBSyncClient) TriggerObjectDrop(typeName string, triggerType TriggerType) error {
+	data, err := cmdb.TypeRead(typeName)
+	if err != nil {
+		return err
+	}
+
+	body := data.GetByPath("body")
+	if !body.IsNull() {
+		body = cmdb.commonTriggersDrop(body, triggerType)
+	}
+
+	return cmdb.TypeUpdate(
+		typeName,
+		body,
+		true,
+	)
+}
+
+func (cmdb CMDBSyncClient) TriggerLinkSet(fromTypeName, toTypeName string, triggerType TriggerType, statefunName ...string) error {
+	if len(statefunName) == 0 {
+		return fmt.Errorf("at least 1 statefun name is required")
+	}
+	body := easyjson.NewJSONObject()
+	body.SetByPath(fmt.Sprintf("triggers.%s", triggerType), easyjson.JSONFromArray(statefunName))
+	return cmdb.TypesLinkUpdate(
+		fromTypeName,
+		toTypeName,
+		nil,
+		body,
+		false,
+	)
+}
+
+func (cmdb CMDBSyncClient) TriggerLinkRemove(fromTypeName, toTypeName string, triggerType TriggerType, statefunName ...string) error {
+	if len(statefunName) == 0 {
+		return fmt.Errorf("at least 1 statefun name is required")
+	}
+
+	data, err := cmdb.TypesLinkRead(fromTypeName, toTypeName)
+	if err != nil {
+		return err
+	}
+
+	tags := []string{}
+	if arr, ok := data.GetByPath("tags").AsArrayString(); ok {
+		tags = arr
+	}
+
+	body := data.GetByPath("body")
+	if !body.IsNull() {
+		body = cmdb.commonTriggerDelete(body, triggerType, statefunName...)
+	}
+
+	return cmdb.TypesLinkUpdate(
+		fromTypeName,
+		toTypeName,
+		tags,
+		body,
+		true,
+	)
+}
+
+func (cmdb CMDBSyncClient) TriggerLinkDrop(fromTypeName, toTypeName string, triggerType TriggerType) error {
+	data, err := cmdb.TypesLinkRead(fromTypeName, toTypeName)
+	if err != nil {
+		return err
+	}
+
+	tags := []string{}
+	if arr, ok := data.GetByPath("tags").AsArrayString(); ok {
+		tags = arr
+	}
+
+	body := data.GetByPath("body")
+	if !body.IsNull() {
+		body = cmdb.commonTriggersDrop(body, triggerType)
+	}
+
+	return cmdb.TypesLinkUpdate(
+		fromTypeName,
+		toTypeName,
+		tags,
+		body,
+		true,
+	)
+}
+
+func (cmdb CMDBSyncClient) TypeCreate(name string, body ...easyjson.JSON) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("body", easyjson.NewJSONObject())
+	if len(body) > 0 {
+		payload.SetByPath("body", body[0])
+	}
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.type.create", name, &payload, nil)))
+}
+
+func (cmdb CMDBSyncClient) TypeUpdate(name string, body easyjson.JSON, replace bool, upsert ...bool) error {
+	payload := easyjson.NewJSONObject()
+	if len(upsert) > 0 {
+		payload.SetByPath("upsert", easyjson.NewJSON(upsert[0]))
+	}
+	payload.SetByPath("replace", easyjson.NewJSON(replace))
+	payload.SetByPath("body", body)
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.type.update", name, &payload, nil)))
+}
+
+func (cmdb CMDBSyncClient) TypeDelete(name string) error {
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.type.delete", name, nil, nil)))
+}
+
+func (cmdb CMDBSyncClient) TypeRead(name string) (easyjson.JSON, error) {
+	om := sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.type.read", name, nil, nil))
+	return om.Data, OpErrorFromOpMsg(om)
+}
+
+func (cmdb CMDBSyncClient) ObjectCreate(objectID, originType string, body ...easyjson.JSON) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("origin_type", easyjson.NewJSON(originType))
+	payload.SetByPath("body", easyjson.NewJSONObject())
+	if len(body) > 0 {
+		payload.SetByPath("body", body[0])
+	}
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.object.create", objectID, &payload, nil)))
+}
+
+func (cmdb CMDBSyncClient) ObjectUdate(objectID string, body easyjson.JSON, replace bool, originType4Upsert ...string) error {
+	payload := easyjson.NewJSONObject()
+	if len(originType4Upsert) > 0 {
+		payload.SetByPath("usert", easyjson.NewJSON(true))
+		payload.SetByPath("origin_type", easyjson.NewJSON(originType4Upsert[0]))
+	}
+	payload.SetByPath("replace", easyjson.NewJSON(replace))
+	payload.SetByPath("body", body)
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.object.update", objectID, &payload, nil)))
+}
+
+func (cmdb CMDBSyncClient) ObjectDelete(id string) error {
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.object.delete", id, nil, nil)))
+}
+
+func (cmdb CMDBSyncClient) ObjectRead(name string) (easyjson.JSON, error) {
+	om := sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.object.read", name, nil, nil))
+	return om.Data, OpErrorFromOpMsg(om)
+}
+
+// ------------------------------------------------------------------------------------------------
+
+func (cmdb CMDBSyncClient) TypesLinkCreate(from, to, objectLinkType string, tags []string, body ...easyjson.JSON) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("to", easyjson.NewJSON(to))
+	payload.SetByPath("body", easyjson.NewJSONObject())
+	if len(body) > 0 {
+		payload.SetByPath("body", body[0])
+	}
+	if tags != nil && len(tags) > 0 {
+		payload.SetByPath("tags", easyjson.JSONFromArray(tags))
+	}
+	payload.SetByPath("object_type", easyjson.NewJSON(objectLinkType))
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.types.link.create", from, &payload, nil)))
+}
+
+func (cmdb CMDBSyncClient) TypesLinkUpdate(from, to string, tags []string, body easyjson.JSON, replace bool, upsert ...bool) error {
+	payload := easyjson.NewJSONObject()
+	if len(upsert) > 0 {
+		payload.SetByPath("upsert", easyjson.NewJSON(upsert[0]))
+	}
+	payload.SetByPath("to", easyjson.NewJSON(to))
+	payload.SetByPath("body", body)
+	if tags != nil && len(tags) > 0 {
+		payload.SetByPath("tags", easyjson.JSONFromArray(tags))
+	}
+	payload.SetByPath("replace", easyjson.NewJSON(replace))
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.types.link.update", from, &payload, nil)))
+}
+
+func (cmdb CMDBSyncClient) TypesLinkDelete(from, to string) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("to", easyjson.NewJSON(to))
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.types.link.delete", from, &payload, nil)))
+}
+
+func (cmdb CMDBSyncClient) TypesLinkRead(from, to string) (easyjson.JSON, error) {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("to", easyjson.NewJSON(to))
+
+	om := sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.types.link.read", from, &payload, nil))
+	return om.Data, OpErrorFromOpMsg(om)
+}
+
+func (cmdb CMDBSyncClient) ObjectsLinkCreate(from, to, name string, tags []string, body ...easyjson.JSON) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("to", easyjson.NewJSON(to))
+	payload.SetByPath("name", easyjson.NewJSON(name))
+	payload.SetByPath("body", easyjson.NewJSONObject())
+	if len(body) > 0 {
+		payload.SetByPath("body", body[0])
+	}
+	if tags != nil && len(tags) > 0 {
+		payload.SetByPath("tags", easyjson.JSONFromArray(tags))
+	}
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.objects.link.create", from, &payload, nil)))
+}
+
+func (cmdb CMDBSyncClient) ObjectsLinkUpdate(from, to string, tags []string, body easyjson.JSON, replace bool, name4Upsert ...string) error {
+	payload := easyjson.NewJSONObject()
+	if len(name4Upsert) > 0 {
+		payload.SetByPath("upsert", easyjson.NewJSON(true))
+		payload.SetByPath("name", easyjson.NewJSON(name4Upsert[0]))
+	}
+	payload.SetByPath("to", easyjson.NewJSON(to))
+	payload.SetByPath("body", body)
+	if tags != nil && len(tags) > 0 {
+		payload.SetByPath("tags", easyjson.JSONFromArray(tags))
+	}
+	payload.SetByPath("replace", easyjson.NewJSON(replace))
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.objects.link.update", from, &payload, nil)))
+}
+
+func (cmdb CMDBSyncClient) ObjectsLinkDelete(from, to string) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("to", easyjson.NewJSON(to))
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.objects.link.delete", from, &payload, nil)))
+}
+
+func (cmdb CMDBSyncClient) ObjectsLinkRead(from, to string) (easyjson.JSON, error) {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("to", easyjson.NewJSON(to))
+
+	om := sfMediators.OpMsgFromSfReply(cmdb.request(sfp.AutoRequestSelect, "functions.cmdb.api.objects.link.read", from, &payload, nil))
+	return om.Data, OpErrorFromOpMsg(om)
+}
+
+// ------------------------------------------------------------------------------------------------
---
+++ clients/go/db/graph.go
@@ -0,0 +1,148 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/foliagecp/easyjson"
+	sfMediators "github.com/foliagecp/sdk/statefun/mediator"
+	sfp "github.com/foliagecp/sdk/statefun/plugins"
+	"github.com/nats-io/nats.go"
+)
+
+type GraphSyncClient struct {
+	request sfp.SFRequestFunc
+}
+
+func NewGraphSyncClient(NatsURL string, NatsRequestTimeout int) (GraphSyncClient, error) {
+	var err error
+	nc, err := nats.Connect(NatsURL)
+	if err != nil {
+		return GraphSyncClient{}, err
+	}
+	request := getRequestFunc(nc, NatsRequestTimeout)
+	return NewGraphSyncClientFromRequestFunction(request)
+}
+
+/*
+ctx.Request
+// or
+runtime.Request
+*/
+func NewGraphSyncClientFromRequestFunction(request sfp.SFRequestFunc) (GraphSyncClient, error) {
+	if request == nil {
+		return GraphSyncClient{}, fmt.Errorf("request must not be nil")
+	}
+	return GraphSyncClient{request: request}, nil
+}
+
+func (gc GraphSyncClient) VertexCreate(id string, body ...easyjson.JSON) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("body", easyjson.NewJSONObject())
+	if len(body) > 0 {
+		payload.SetByPath("body", body[0])
+	}
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(gc.request(sfp.AutoRequestSelect, "functions.graph.api.vertex.create", id, &payload, nil)))
+}
+
+func (gc GraphSyncClient) VertexUpdate(id string, body easyjson.JSON, replace bool, upsert ...bool) error {
+	payload := easyjson.NewJSONObject()
+	if len(upsert) > 0 {
+		payload.SetByPath("upsert", easyjson.NewJSON(upsert[0]))
+	}
+	payload.SetByPath("replace", easyjson.NewJSON(replace))
+	payload.SetByPath("body", body)
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(gc.request(sfp.AutoRequestSelect, "functions.graph.api.vertex.update", id, &payload, nil)))
+}
+
+func (gc GraphSyncClient) VertexDelete(id string) error {
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(gc.request(sfp.AutoRequestSelect, "functions.graph.api.vertex.delete", id, nil, nil)))
+}
+
+func (gc GraphSyncClient) VertexRead(id string) (easyjson.JSON, error) {
+	om := sfMediators.OpMsgFromSfReply(gc.request(sfp.AutoRequestSelect, "functions.graph.api.vertex.delete", id, nil, nil))
+	return om.Data, OpErrorFromOpMsg(om)
+}
+
+func (gc GraphSyncClient) VerticesLinkCreate(from, to, linkName, linkType string, tags []string, body ...easyjson.JSON) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("to", easyjson.NewJSON(to))
+	payload.SetByPath("name", easyjson.NewJSON(linkName))
+	payload.SetByPath("type", easyjson.NewJSON(linkType))
+	payload.SetByPath("body", easyjson.NewJSONObject())
+	if len(body) > 0 {
+		payload.SetByPath("body", body[0])
+	}
+	if tags != nil && len(tags) > 0 {
+		payload.SetByPath("tags", easyjson.JSONFromArray(tags))
+	}
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(gc.request(sfp.AutoRequestSelect, "functions.graph.api.link.create", from, &payload, nil)))
+}
+
+func (gc GraphSyncClient) VerticesLinkUpdate(from, linkName string, tags []string, body easyjson.JSON, replace bool, toAndType4Upsert ...string) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("name", easyjson.NewJSON(linkName))
+	payload.SetByPath("body", body)
+	if tags != nil && len(tags) > 0 {
+		payload.SetByPath("tags", easyjson.JSONFromArray(tags))
+	}
+	if len(toAndType4Upsert) == 2 {
+		payload.SetByPath("upsert", easyjson.NewJSON(true))
+		payload.SetByPath("to", easyjson.NewJSON(toAndType4Upsert[0]))
+		payload.SetByPath("type", easyjson.NewJSON(toAndType4Upsert[1]))
+	} else {
+		return fmt.Errorf("toAndType4Upsert must consist of 2 string values: \"to\" and \"type\"")
+	}
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(gc.request(sfp.AutoRequestSelect, "functions.graph.api.link.update", from, &payload, nil)))
+}
+
+func (gc GraphSyncClient) VerticesLinkUpdateByToAndType(from, to, linkType string, tags []string, body easyjson.JSON, replace bool, name4Upsert ...string) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("to", easyjson.NewJSON(to))
+	payload.SetByPath("type", easyjson.NewJSON(linkType))
+
+	payload.SetByPath("body", body)
+	if tags != nil && len(tags) > 0 {
+		payload.SetByPath("tags", easyjson.JSONFromArray(tags))
+	}
+	if len(name4Upsert) > 0 {
+		payload.SetByPath("upsert", easyjson.NewJSON(true))
+		payload.SetByPath("name", easyjson.NewJSON(name4Upsert[0]))
+	}
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(gc.request(sfp.AutoRequestSelect, "functions.graph.api.link.update", from, &payload, nil)))
+}
+
+func (cc CMDBSyncClient) VerticesLinkDelete(from, linkName string) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("name", easyjson.NewJSON(linkName))
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cc.request(sfp.AutoRequestSelect, "functions.graph.api.link.delete", from, &payload, nil)))
+}
+
+func (cc CMDBSyncClient) VerticesLinkDeleteByToAndType(from, to, linkType string) error {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("to", easyjson.NewJSON(to))
+	payload.SetByPath("type", easyjson.NewJSON(linkType))
+
+	return OpErrorFromOpMsg(sfMediators.OpMsgFromSfReply(cc.request(sfp.AutoRequestSelect, "functions.graph.api.link.delete", from, &payload, nil)))
+}
+
+func (cc CMDBSyncClient) VerticesLinkRead(from, linkName string) (easyjson.JSON, error) {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("name", easyjson.NewJSON(linkName))
+
+	om := sfMediators.OpMsgFromSfReply(cc.request(sfp.AutoRequestSelect, "functions.graph.api.link.read", from, &payload, nil))
+	return om.Data, OpErrorFromOpMsg(om)
+}
+
+func (cc CMDBSyncClient) VerticesLinkReadByToAndType(from, to, linkType string) (easyjson.JSON, error) {
+	payload := easyjson.NewJSONObject()
+	payload.SetByPath("to", easyjson.NewJSON(to))
+	payload.SetByPath("type", easyjson.NewJSON(linkType))
+
+	om := sfMediators.OpMsgFromSfReply(cc.request(sfp.AutoRequestSelect, "functions.graph.api.link.read", from, &payload, nil))
+	return om.Data, OpErrorFromOpMsg(om)
+}
