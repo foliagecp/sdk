75f12a9c825a0a661680359570b96331ded64414
Andrei Shipov <andreishipov@hotmail.com>
2025-01-18 18:06:30 +0900

    update: jpgql has backpressure parameterization + stats; stats data updated

--- embedded/graph/jpgql/jpgql.go
+++ embedded/graph/jpgql/jpgql.go
@@ -49,25 +49,31 @@ Request:
 		query: string - required // Json path query
 
 	options: json - optional
-		qds_timeout_sec: float - optional // Query Depth Spreading timeout (whole query timeout will be twice longer), default = 5
+		qds_timeout_sec: float - optional // Query Depth Spreading timeout (whole query timeout will be about twice longer), default = 5
 		max_depth: int - optional // default = -1
-		started_nano: int // set by system from initial moment, will be overwritted if received
+		bp_timeout_ms: float // Back pressure timeout ms, default = 300
+
+		query_started_nano: int // set by system from initial moment, will be overwritted if received
+		call_started_nano: int // set by system for each call, will be overwritted if received
 		depth: int // set by system from initial moment, will be overwritted if received
 */
 
 func JPGQLCallTreeResultAggregation(_ sfPlugins.StatefunExecutor, ctx *sfPlugins.StatefunContextProcessor) {
-	createReturnData := func(uuids map[string]bool, currentDepth, skippedByDepth, skippedByTimeout, verticesPassed int, queryStartTimeNano int64) easyjson.JSON {
+	callTimeNano := time.Now().UnixNano()
+	createReturnData := func(uuids map[string]bool, currentDepth, skippedByDepth, skippedByTimeout, skippedByBP, verticesPassed int, queryStartTimeNano int64, maxBPDurationNano int64) easyjson.JSON {
 		resultUUIDS := map[string]bool{}
 		if uuids != nil {
 			resultUUIDS = uuids
 		}
 		returnData := easyjson.NewJSONObjectWithKeyValue("uuids", easyjson.NewJSON(resultUUIDS))
-		returnData.SetByPath("map_reduce_stats.max_depth_reached", easyjson.NewJSON(currentDepth))
-		returnData.SetByPath("map_reduce_stats.paths_skipped_by_depth", easyjson.NewJSON(skippedByDepth))
-		returnData.SetByPath("map_reduce_stats.paths_skipped_by_timeout", easyjson.NewJSON(skippedByTimeout))
-		returnData.SetByPath("map_reduce_stats.vertices_passed", easyjson.NewJSON(verticesPassed))
-		returnData.SetByPath("map_reduce_stats.qds_end_time_nano", easyjson.NewJSON(time.Now().UnixNano()))
-		returnData.SetByPath("map_reduce_stats.query_start_time_nano", easyjson.NewJSON(queryStartTimeNano))
+		returnData.SetByPath("stats.paths_skipped.depth", easyjson.NewJSON(skippedByDepth))
+		returnData.SetByPath("stats.paths_skipped.timeout", easyjson.NewJSON(skippedByTimeout))
+		returnData.SetByPath("stats.paths_skipped.backpressure", easyjson.NewJSON(skippedByBP))
+		returnData.SetByPath("stats.call_tree.max_depth_reached", easyjson.NewJSON(currentDepth))
+		returnData.SetByPath("stats.call_tree.vertices_passed", easyjson.NewJSON(verticesPassed))
+		returnData.SetByPath("stats.times.qds_end_nano", easyjson.NewJSON(callTimeNano))
+		returnData.SetByPath("stats.times.query_start_nano", easyjson.NewJSON(queryStartTimeNano))
+		returnData.SetByPath("stats.duration.max_backpressure_nano", easyjson.NewJSON(maxBPDurationNano))
 		return returnData
 	}
 	aggregateData := func(returnData, aggregatedReturnData easyjson.JSON) easyjson.JSON {
@@ -82,47 +88,57 @@ func JPGQLCallTreeResultAggregation(_ sfPlugins.StatefunExecutor, ctx *sfPlugins
 		}
 		// ------------------------------------------------
 		// Map's reduce -----------------------------------
-		retMDR := returnData.GetByPath("map_reduce_stats.max_depth_reached").AsNumericDefault(0)
-		aggMDR := aggregatedReturnData.GetByPath("map_reduce_stats.max_depth_reached").AsNumericDefault(0)
+		retMDR := returnData.GetByPath("stats.call_tree.max_depth_reached").AsNumericDefault(0)
+		aggMDR := aggregatedReturnData.GetByPath("stats.call_tree.max_depth_reached").AsNumericDefault(0)
 		if aggMDR > retMDR {
 			retMDR = aggMDR
 		}
 
-		retPSBD := returnData.GetByPath("map_reduce_stats.paths_skipped_by_depth").AsNumericDefault(0)
-		aggPSBD := aggregatedReturnData.GetByPath("map_reduce_stats.paths_skipped_by_depth").AsNumericDefault(0)
+		retPSBD := returnData.GetByPath("stats.paths_skipped.depth").AsNumericDefault(0)
+		aggPSBD := aggregatedReturnData.GetByPath("stats.paths_skipped.depth").AsNumericDefault(0)
 		retPSBD += aggPSBD
 
-		retPSBT := returnData.GetByPath("map_reduce_stats.paths_skipped_by_timeout").AsNumericDefault(0)
-		aggPSBT := aggregatedReturnData.GetByPath("map_reduce_stats.paths_skipped_by_timeout").AsNumericDefault(0)
+		retPSBT := returnData.GetByPath("stats.paths_skipped.timeout").AsNumericDefault(0)
+		aggPSBT := aggregatedReturnData.GetByPath("stats.paths_skipped.timeout").AsNumericDefault(0)
 		retPSBT += aggPSBT
 
-		retVP := returnData.GetByPath("map_reduce_stats.vertices_passed").AsNumericDefault(0)
-		aggVP := aggregatedReturnData.GetByPath("map_reduce_stats.vertices_passed").AsNumericDefault(0)
+		retPSBBP := returnData.GetByPath("stats.paths_skipped.backpressure").AsNumericDefault(0)
+		aggPSBBP := aggregatedReturnData.GetByPath("stats.paths_skipped.backpressure").AsNumericDefault(0)
+		retPSBBP += aggPSBBP
+
+		retVP := returnData.GetByPath("stats.call_tree.vertices_passed").AsNumericDefault(0)
+		aggVP := aggregatedReturnData.GetByPath("stats.call_tree.vertices_passed").AsNumericDefault(0)
 		retVP += aggVP
 
-		retQETN := returnData.GetByPath("map_reduce_stats.qds_end_time_nano").AsNumericDefault(0)
-		aggQETN := aggregatedReturnData.GetByPath("map_reduce_stats.qds_end_time_nano").AsNumericDefault(0)
+		retQETN := returnData.GetByPath("stats.times.qds_end_nano").AsNumericDefault(0)
+		aggQETN := aggregatedReturnData.GetByPath("stats.times.qds_end_nano").AsNumericDefault(0)
 		if aggQETN > retQETN {
 			retQETN = aggQETN
 		}
 
-		aggQSTN := aggregatedReturnData.GetByPath("map_reduce_stats.query_start_time_nano").AsNumericDefault(0)
+		retMBPDN := returnData.GetByPath("stats.duration.max_backpressure_nano").AsNumericDefault(0)
+		aggMBPDN := aggregatedReturnData.GetByPath("stats.duration.max_backpressure_nano").AsNumericDefault(0)
+		if aggMBPDN > retMBPDN {
+			retMBPDN = aggMBPDN
+		}
+
+		aggQSTN := aggregatedReturnData.GetByPath("stats.times.query_start_nano").AsNumericDefault(0)
 		// ------------------------------------------------
 
-		thisTimeNano := time.Now().UnixNano()
-
 		newReturnData := easyjson.NewJSONObjectWithKeyValue("uuids", resultUUIDs)
-		newReturnData.SetByPath("map_reduce_stats.max_depth_reached", easyjson.NewJSON(retMDR))
-		newReturnData.SetByPath("map_reduce_stats.paths_skipped_by_depth", easyjson.NewJSON(retPSBD))
-		newReturnData.SetByPath("map_reduce_stats.paths_skipped_by_timeout", easyjson.NewJSON(retPSBT))
-		newReturnData.SetByPath("map_reduce_stats.vertices_passed", easyjson.NewJSON(retVP))
-		newReturnData.SetByPath("map_reduce_stats.qds_end_time_nano", easyjson.NewJSON(retQETN))
-		newReturnData.SetByPath("map_reduce_stats.query_start_time_nano", easyjson.NewJSON(aggQSTN))
-		newReturnData.SetByPath("map_reduce_stats.query_end_time_nano", easyjson.NewJSON(thisTimeNano))
+		newReturnData.SetByPath("stats.paths_skipped.depth", easyjson.NewJSON(retPSBD))
+		newReturnData.SetByPath("stats.paths_skipped.timeout", easyjson.NewJSON(retPSBT))
+		newReturnData.SetByPath("stats.paths_skipped.backpressure", easyjson.NewJSON(retPSBBP))
+		newReturnData.SetByPath("stats.call_tree.vertices_passed", easyjson.NewJSON(retVP))
+		newReturnData.SetByPath("stats.call_tree.max_depth_reached", easyjson.NewJSON(retMDR))
+		newReturnData.SetByPath("stats.times.qds_end_nano", easyjson.NewJSON(retQETN))
+		newReturnData.SetByPath("stats.times.query_start_nano", easyjson.NewJSON(aggQSTN))
+		newReturnData.SetByPath("stats.times.query_end_nano", easyjson.NewJSON(callTimeNano))
+		newReturnData.SetByPath("stats.duration.max_backpressure_nano", easyjson.NewJSON(retMBPDN))
 
-		newReturnData.SetByPath("map_reduce_stats.query_duration_nano", easyjson.NewJSON(thisTimeNano-int64(aggQSTN)))
-		newReturnData.SetByPath("map_reduce_stats.qds_duration_nano", easyjson.NewJSON(retQETN-aggQSTN))
-		newReturnData.SetByPath("map_reduce_stats.agg_duration_nano", easyjson.NewJSON(thisTimeNano-int64(retQETN)))
+		newReturnData.SetByPath("stats.duration.query_nano", easyjson.NewJSON(callTimeNano-int64(aggQSTN)))
+		newReturnData.SetByPath("stats.duration.qds_nano", easyjson.NewJSON(retQETN-aggQSTN))
+		newReturnData.SetByPath("stats.duration.agg_nano", easyjson.NewJSON(callTimeNano-int64(retQETN)))
 
 		return newReturnData
 	}
@@ -148,26 +164,43 @@ func JPGQLCallTreeResultAggregation(_ sfPlugins.StatefunExecutor, ctx *sfPlugins
 	switch om.GetOpType() {
 	case mediator.MereOp: // Initial call of jpgql
 		newOptions := ctx.Options
-		newOptions.SetByPath("started_nano", easyjson.NewJSON(time.Now().UnixNano()))
 		queryDepthSpreadTimeoutSec := ctx.Options.GetByPath("qds_timeout_sec").AsNumericDefault(5)
 		newOptions.SetByPath("qds_timeout_sec", easyjson.NewJSON(queryDepthSpreadTimeoutSec))
+		queryMaxDepth := ctx.Options.GetByPath("max_depth").AsNumericDefault(-1)
+		newOptions.SetByPath("max_depth", easyjson.NewJSON(queryMaxDepth))
+		bpTimeoutMs := ctx.Options.GetByPath("bp_timeout_ms").AsNumericDefault(300)
+		newOptions.SetByPath("bp_timeout_ms", easyjson.NewJSON(bpTimeoutMs))
+
+		newOptions.SetByPath("query_started_nano", easyjson.NewJSON(callTimeNano))
+		newOptions.SetByPath("call_started_nano", easyjson.NewJSON(callTimeNano))
 		newOptions.SetByPath("depth", easyjson.NewJSON(0))
 		system.MsgOnErrorReturn(om.SignalWithAggregation(sfPlugins.JetstreamGlobalSignal, ctx.Self.Typename, vId+"==="+pId, ctx.Payload, newOptions))
 	case mediator.WorkerIsTaskedByAggregatorOp:
-		maxDepth := int(ctx.Options.GetByPath("max_depth").AsNumericDefault(-1))
+		qdsTimeoutSec := float64(ctx.Options.GetByPath("qds_timeout_sec").AsNumericDefault(0))
+		maxDepth := int(ctx.Options.GetByPath("max_depth").AsNumericDefault(0))
+		bpTimeoutMs := float64(ctx.Options.GetByPath("bp_timeout_ms").AsNumericDefault(0))
+
 		currentDepth := int(ctx.Options.GetByPath("depth").AsNumericDefault(0))
-		qdsTimeoutSec := float64(ctx.Options.GetByPath("qds_timeout_sec").AsNumericDefault(5))
-		startedNano := int64(ctx.Options.GetByPath("started_nano").AsNumericDefault(-1))
+		queryStartedNano := int64(ctx.Options.GetByPath("query_started_nano").AsNumericDefault(-1))
+		callStartedNano := int64(ctx.Options.GetByPath("call_started_nano").AsNumericDefault(-1))
+		bpNano := callTimeNano - callStartedNano
 
 		// Checking limits --------------------------------------------------------------
+		if callStartedNano > 0 {
+			if bpNano > int64(bpTimeoutMs*float64(time.Millisecond)) { // One jpgql execution takes too long - back pressure
+				data := createReturnData(nil, currentDepth, 0, 0, 1, 0, queryStartedNano, bpNano)
+				om.AggregateOpMsg(sfMediators.OpMsgOk(data)).Reply()
+				return
+			}
+		}
 		if maxDepth >= 0 && currentDepth >= maxDepth { // Will be cheking at depth + 1 if this condition returns false, thus currentDepth !!!>=!!! maxDepth
-			data := createReturnData(nil, currentDepth, 1, 0, 0, startedNano)
+			data := createReturnData(nil, currentDepth, 1, 0, 0, 0, queryStartedNano, bpNano)
 			om.AggregateOpMsg(sfMediators.OpMsgOk(data)).Reply()
 			return
 		}
-		if startedNano > 0 {
-			if time.Now().UnixNano()-startedNano > int64(qdsTimeoutSec*float64(time.Second)) {
-				data := createReturnData(nil, currentDepth, 0, 1, 0, startedNano)
+		if queryStartedNano > 0 {
+			if callTimeNano-queryStartedNano > int64(qdsTimeoutSec*float64(time.Second)) {
+				data := createReturnData(nil, currentDepth, 0, 1, 0, 0, queryStartedNano, bpNano)
 				om.AggregateOpMsg(sfMediators.OpMsgOk(data)).Reply()
 				return
 			}
@@ -206,6 +239,7 @@ func JPGQLCallTreeResultAggregation(_ sfPlugins.StatefunExecutor, ctx *sfPlugins
 
 					newOptions := ctx.Options.Clone()
 					newOptions.SetByPath("depth", easyjson.NewJSON(currentDepth+1))
+					newOptions.SetByPath("call_started_nano", easyjson.NewJSON(callTimeNano))
 					err := om.SignalWithAggregation(sfPlugins.JetstreamGlobalSignal, ctx.Self.Typename, objectID+"==="+pId, &workerPayload, &newOptions)
 					if err != nil {
 						om.AggregateOpMsg(sfMediators.OpMsgFailed(err.Error())).Reply()
@@ -216,12 +250,12 @@ func JPGQLCallTreeResultAggregation(_ sfPlugins.StatefunExecutor, ctx *sfPlugins
 			}
 			if workersToAggregateFrom == 0 { // This vertex' children ALL are leafs against query
 				checkedVerticesInThisCall := 1 + len(resultObjects) // This vertex + all vertices-leafs
-				data := createReturnData(objectsToReturnAsAResult, currentDepth+1, 0, 0, checkedVerticesInThisCall, startedNano)
+				data := createReturnData(objectsToReturnAsAResult, currentDepth+1, 0, 0, 0, checkedVerticesInThisCall, queryStartedNano, bpNano)
 				om.AggregateOpMsg(sfMediators.OpMsgOk(data)).Reply()
 				return
 			} else { // This vertex' children SOME are leafs against query and SOME are tasked so this one waits for AggregatedWorkersOp
 				checkedVerticesInThisCall := 1 + (len(resultObjects) - workersToAggregateFrom) // This vertex + all vertices-leafs
-				data := createReturnData(objectsToReturnAsAResult, currentDepth+1, 0, 0, checkedVerticesInThisCall, startedNano)
+				data := createReturnData(objectsToReturnAsAResult, currentDepth+1, 0, 0, 0, checkedVerticesInThisCall, queryStartedNano, bpNano)
 				om.AddIntermediateResult(ctx, &data)
 			}
 		}
