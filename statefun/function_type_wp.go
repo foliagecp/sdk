d393f58611d74c612b5c0f876960973ad6ebdb9c
Andrei Shipov <andreishipov@hotmail.com>
2025-04-21 20:10:09 +0900

    update: workers pull added to fix statefun execution performance and routines leak

---
+++ statefun/function_type_wp.go
@@ -0,0 +1,156 @@
+package statefun
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/foliagecp/easyjson"
+	sfPlugins "github.com/foliagecp/sdk/statefun/plugins"
+)
+
+type SFWorkerMessage struct {
+	ID   string
+	Data FunctionTypeMsg
+}
+
+type SFWorkerTask struct {
+	Ft  *FunctionType
+	Msg SFWorkerMessage
+}
+
+// SFWorkerPool - controls the statefun pool
+type SFWorkerPool struct {
+	taskQueue   chan SFWorkerTask
+	maxWorkers  int
+	idleTimeout time.Duration
+
+	mu          sync.Mutex
+	workers     int
+	idleWorkers int
+
+	stopCh  chan struct{}
+	stopped bool
+
+	wg sync.WaitGroup
+}
+
+func NewSFWorkerPool(maxWorkers int, idleTimeout time.Duration, taskQueueLen int) *SFWorkerPool {
+	return &SFWorkerPool{
+		taskQueue:   make(chan SFWorkerTask, taskQueueLen),
+		maxWorkers:  maxWorkers,
+		idleTimeout: idleTimeout,
+		stopCh:      make(chan struct{}),
+	}
+}
+
+func (wp *SFWorkerPool) Submit(task SFWorkerTask) {
+	wp.mu.Lock()
+	if wp.stopped {
+		wp.mu.Unlock()
+		return
+	}
+
+	hasIdle := wp.idleWorkers > 0
+	canGrow := wp.workers < wp.maxWorkers
+	if !hasIdle && canGrow {
+		wp.workers++
+		fmt.Println(">>>>>>>>>>>>>> WP GROW:", wp.workers)
+		wp.wg.Add(1)
+		wp.mu.Unlock()
+		go wp.worker()
+	} else {
+		wp.mu.Unlock()
+	}
+
+	select {
+	case wp.taskQueue <- task:
+	case <-wp.stopCh:
+	}
+}
+
+func (wp *SFWorkerPool) worker() {
+	defer func() {
+		wp.mu.Lock()
+		wp.workers--
+		wp.mu.Unlock()
+	}()
+
+	timer := time.NewTimer(wp.idleTimeout)
+	defer timer.Stop()
+
+	for {
+		wp.mu.Lock()
+		wp.idleWorkers++
+		wp.mu.Unlock()
+
+		select {
+		case task := <-wp.taskQueue:
+			wp.mu.Lock()
+			wp.idleWorkers--
+			wp.mu.Unlock()
+
+			{
+				ft := task.Ft
+				id := task.Msg.ID
+				typenameIDContextProcessor := sfPlugins.StatefunContextProcessor{
+					GetFunctionContext:        func() *easyjson.JSON { return ft.getContext(ft.name + "." + id) },
+					SetFunctionContext:        func(context *easyjson.JSON) { ft.setContext(ft.name+"."+id, context) },
+					SetContextExpirationAfter: func(after time.Duration) { ft.setContextExpirationAfter(ft.name+"."+id, after) },
+					GetObjectContext:          func() *easyjson.JSON { return ft.getContext(id) },
+					SetObjectContext:          func(context *easyjson.JSON) { ft.setContext(id, context) },
+					Domain:                    ft.runtime.Domain,
+					Self:                      sfPlugins.StatefunAddress{Typename: ft.name, ID: id},
+					Signal: func(signalProvider sfPlugins.SignalProvider, targetTypename string, targetID string, j *easyjson.JSON, o *easyjson.JSON) error {
+						return ft.runtime.signal(signalProvider, ft.name, id, targetTypename, targetID, j, o)
+					},
+					Request: func(requestProvider sfPlugins.RequestProvider, targetTypename string, targetID string, j *easyjson.JSON, o *easyjson.JSON, timeout ...time.Duration) (*easyjson.JSON, error) {
+						return ft.runtime.request(requestProvider, ft.name, id, targetTypename, targetID, j, o)
+					},
+					Egress: func(egressProvider sfPlugins.EgressProvider, j *easyjson.JSON, customId ...string) error {
+						egressId := id
+						if len(customId) > 0 {
+							egressId = customId[0]
+						}
+						return ft.runtime.egress(egressProvider, ft.name, egressId, j)
+					},
+					// To be assigned later:
+					// Call: ...
+					// Payload: ...
+					// Options: ... // Otions from initial typename declaration will be merged and overwritten by the incoming one in message
+					// Caller: ...
+				}
+
+				task.Ft.handleMsgForID(id, task.Msg.Data, &typenameIDContextProcessor)
+				ft.idKeyMutex.Unlock(id)
+			}
+
+			if !timer.Stop() {
+				<-timer.C
+			}
+			timer.Reset(wp.idleTimeout)
+
+		case <-timer.C:
+			wp.mu.Lock()
+			wp.idleWorkers--
+			fmt.Println(">>>>>>>>>>>>>> ---- WP SHRINK:", wp.workers)
+			wp.mu.Unlock()
+			return
+		case <-wp.stopCh:
+			return
+		}
+	}
+}
+
+func (wp *SFWorkerPool) Stop() {
+	wp.mu.Lock()
+	if wp.stopped {
+		wp.mu.Unlock()
+		return
+	}
+	wp.stopped = true
+	close(wp.stopCh)
+	wp.mu.Unlock()
+
+	wp.wg.Wait()
+}
--- statefun/runtime.go
+++ statefun/runtime.go
@@ -10,10 +10,10 @@ import (
 	lg "github.com/foliagecp/sdk/statefun/logger"
 	"github.com/foliagecp/sdk/statefun/system"
 	"github.com/nats-io/nats.go"
+	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/foliagecp/sdk/statefun/cache"
 	sfPlugins "github.com/foliagecp/sdk/statefun/plugins"
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 type OnAfterStartFunction func(ctx context.Context, runtime *Runtime) error
@@ -37,6 +37,8 @@ type Runtime struct {
 	glce int64 // Global last call ended - time of last call of last function handling id of any function type
 	gc   int64 // Global counter - max total id handlers for all function types
 
+	sfWorkerPool *SFWorkerPool
+
 	shutdown chan struct{}
 	wg       sync.WaitGroup
 }
@@ -47,6 +49,7 @@ func NewRuntime(config RuntimeConfig) (*Runtime, error) {
 		config:                  config,
 		registeredFunctionTypes: make(map[string]*FunctionType),
 		shutdown:                make(chan struct{}),
+		sfWorkerPool:            NewSFWorkerPool(1000, 5*time.Second, 1000),
 	}
 
 	var err error
