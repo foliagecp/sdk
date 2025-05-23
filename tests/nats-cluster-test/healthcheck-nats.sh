ec5f8515f39e8568c8da34ac0f9dc0b706dc6373
atauov <atauov@gmail.com>
2025-05-23 18:28:06 +0500

    active-passive mode

--- tests/nats-cluster-test/healthcheck-nats.sh
+++ tests/nats-cluster-test/healthcheck-nats.sh
@@ -2,8 +2,14 @@
 # healthcheck-nats.sh
 
 if [ -z "$NATS_URL" ]; then
-  echo "NATS_URL is not set"
-  exit 1
+    echo "NATS_URL is not set"
+    exit 1
 fi
 
 nats --server="$NATS_URL" server check jetstream | grep -q "OK JetStream"
+if [ $? -eq 0 ]; then
+    exit 0
+else
+    echo "JetStream check failed"
+    exit 1
+fi
