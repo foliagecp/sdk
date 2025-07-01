d3e0eb224ec3eff46b1c247d9193f355dca18464
atauov <atauov@gmail.com>
2025-07-01 10:48:26 +0500

    write barrier

---
+++ statefun/cache/backup_write_barrier.go
@@ -0,0 +1,130 @@
+package cache
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/foliagecp/easyjson"
+	customNatsKv "github.com/foliagecp/sdk/embedded/nats/kv"
+	lg "github.com/foliagecp/sdk/statefun/logger"
+	"github.com/foliagecp/sdk/statefun/system"
+	"github.com/nats-io/nats.go"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	BackupLockKey = "__backup_lock_"
+
+	BarrierStatusUnlocked = 0
+	BarrierStatusLocked   = 1
+
+	BarrierCheckInterval = 5 * time.Second
+)
+
+func (cs *Store) getBackupBarrierInfo() (*easyjson.JSON, error) {
+	entry, err := customNatsKv.KVGet(cs.js, cs.kv, BackupLockKey)
+	if err != nil {
+		if errors.Is(err, nats.ErrKeyNotFound) {
+			barrier := easyjson.NewJSONObject()
+			barrier.SetByPath("status", easyjson.NewJSON(BarrierStatusUnlocked))
+			return barrier.GetPtr(), nil
+		}
+		return nil, err
+	}
+
+	if barrier, ok := easyjson.JSONFromBytes(entry.Value()); ok {
+		return &barrier, nil
+	}
+
+	return nil, fmt.Errorf("failed to parse backup barrier JSON")
+}
+
+func (cs *Store) waitForBackupBarrierRemoval() error {
+	le := lg.GetLogger()
+	le.Debug(context.TODO(), "Write operation blocked by backup barrier")
+
+	ticker := time.NewTicker(BarrierCheckInterval)
+	defer ticker.Stop()
+
+	//TODO add timeout?
+	for {
+		select {
+		case <-cs.ctx.Done():
+			return nil
+
+		case <-ticker.C:
+			barrier, err := cs.getBackupBarrierInfo()
+			if err != nil {
+				le.Errorf(context.TODO(), "failed to get backup barrier info: %v", err)
+				continue
+			}
+			if int32(barrier.GetByPath("status").AsNumericDefault(BarrierStatusUnlocked)) == BarrierStatusUnlocked {
+				return nil
+			}
+		}
+	}
+}
+
+func (cs *Store) checkStoreValueBeforeBarrierSynced(sv *StoreValue, barrierTs int64) bool {
+	if sv == nil {
+		return true
+	}
+
+	sv.Lock("checkStoreValueBeforeBarrierSynced")
+	defer sv.Unlock("checkStoreValueBeforeBarrierSynced")
+
+	if sv.valueExists && sv.valueUpdateTime <= barrierTs {
+		if sv.syncNeeded || !sv.syncedWithKV {
+			return false
+		}
+	}
+
+	for _, childSv := range sv.store {
+		if !cs.checkStoreValueBeforeBarrierSynced(childSv, barrierTs) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (cs *Store) checkBarrierInfoBeforeWrite(opTime int64) error {
+	if cs.shouldRefreshBarrier() {
+		cs.refreshBarrierFromKV()
+	}
+
+	barrierTime, status := cs.getBarrierState()
+	if status == BarrierStatusLocked {
+		if opTime > barrierTime {
+			return fmt.Errorf("operation blocked by barrier: %d > %d", opTime, barrierTime)
+		}
+	}
+
+	return nil
+}
+
+func (cs *Store) getBarrierState() (timestamp int64, status int32) {
+	return atomic.LoadInt64(&cs.barrierTimestamp), atomic.LoadInt32(&cs.barrierStatus)
+}
+
+func (cs *Store) shouldRefreshBarrier() bool {
+	currentTime := system.GetCurrentTimeNs()
+	lastChecked := atomic.LoadInt64(&cs.barrierLastChecked)
+
+	return (currentTime - lastChecked) > BarrierCheckInterval.Nanoseconds()
+}
+
+func (cs *Store) refreshBarrierFromKV() {
+	barrier, err := cs.getBackupBarrierInfo()
+	if err != nil {
+		return
+	}
+
+	barrierTs := int64(barrier.GetByPath("barrier_timestamp").AsNumericDefault(0))
+	status := int32(barrier.GetByPath("status").AsNumericDefault(BarrierStatusUnlocked))
+
+	atomic.StoreInt64(&cs.barrierTimestamp, barrierTs)
+	atomic.StoreInt32(&cs.barrierStatus, status)
+	atomic.StoreInt64(&cs.barrierLastChecked, system.GetCurrentTimeNs())
+}
