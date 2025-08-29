32414fcde8f4a64f12b8ccf3700a6965fd9e6fb7
Andrei Shipov <andreishipov@hotmail.com>
2025-08-29 21:55:12 +0900

    fix: LLAPIVertexDelete mutex lock

--- embedded/graph/crud/ll_crud.go
+++ embedded/graph/crud/ll_crud.go
@@ -294,8 +294,6 @@ func LLAPIVertexDelete(_ sfPlugins.StatefunExecutor, ctx *sfPlugins.StatefunCont
 
 	opStack := getOpStackFromOptions(ctx.Options)
 
-	operationKeysMutexLock(ctx, []string{selfID}, true)
-
 	// Delete all out links -------------------------------
 	outLinkKeys := ctx.Domain.Cache().GetKeysByPattern(fmt.Sprintf(OutLinkBodyKeyPrefPattern+KeySuff1Pattern, selfID, ">"))
 	for _, outLinkKey := range outLinkKeys {
@@ -309,7 +307,6 @@ func LLAPIVertexDelete(_ sfPlugins.StatefunExecutor, ctx *sfPlugins.StatefunCont
 		om.AggregateOpMsg(sfMediators.OpMsgFromSfReply(ctx.Request(sfPlugins.AutoRequestSelect, "functions.graph.api.link.delete", makeSequenceFreeParentBasedID(ctx, selfID), injectParentHoldsLocks(ctx, &deleteLinkPayload), ctx.Options)))
 		mergeOpStack(opStack, om.GetLastSyncOp().Data.GetByPath("op_stack").GetPtr())
 		if om.GetLastSyncOp().Status == sfMediators.SYNC_OP_STATUS_FAILED {
-			operationKeysMutexUnlock(ctx)
 			system.MsgOnErrorReturn(om.ReplyWithData(resultWithOpStack(nil, opStack).GetPtr()))
 			return
 		}
@@ -330,12 +327,13 @@ func LLAPIVertexDelete(_ sfPlugins.StatefunExecutor, ctx *sfPlugins.StatefunCont
 		om.AggregateOpMsg(sfMediators.OpMsgFromSfReply(ctx.Request(sfPlugins.AutoRequestSelect, "functions.graph.api.link.delete", makeSequenceFreeParentBasedID(ctx, fromObjectID), injectParentHoldsLocks(ctx, &deleteLinkPayload), ctx.Options)))
 		mergeOpStack(opStack, om.GetLastSyncOp().Data.GetByPath("op_stack").GetPtr())
 		if om.GetLastSyncOp().Status == sfMediators.SYNC_OP_STATUS_FAILED {
-			operationKeysMutexUnlock(ctx)
 			system.MsgOnErrorReturn(om.ReplyWithData(resultWithOpStack(nil, opStack).GetPtr()))
 			return
 		}
 	}
 	// ----------------------------------------------------
+
+	operationKeysMutexLock(ctx, []string{selfID}, true)
 
 	var oldBody *easyjson.JSON = nil
 	if opStack != nil {
@@ -343,6 +341,7 @@ func LLAPIVertexDelete(_ sfPlugins.StatefunExecutor, ctx *sfPlugins.StatefunCont
 	}
 
 	ctx.Domain.Cache().DeleteValue(selfID, true, opTime, "") // Delete vertex's body
+
 	indexRemoveVertexBody(ctx)
 
 	operationKeysMutexUnlock(ctx)
